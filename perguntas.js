487748fee36686f0ff780ef94eb862b22bed95e1
jjsz23 <sarinhaferrari432@gmail.com>
2024-11-21 15:24:42 -0300

    c

---
+++ perguntas.js
@@ -0,0 +1,38 @@
+criaCartao(
+    'teologia',
+    'qual a base da teologia?',
+    'a fé. nao tem como você estudar sobre algo que nao acredita'
+)
+
+criaCartao(
+    'Jesus',
+    'qual foi a ultima pregação de jesus?',
+    'Sua ultima pregação foi naquela cruz, e essa pregação nunca teve fim, pois através daquele amor demonstrado no gólgota, que morreu por nossos pecados'
+)
+
+criaCartao(
+    'sobre Jacó (gênesis 28.18-19)',
+    'qual foi o nome que Jacó deu ao lugar que ele sonhou com Deus ?',
+    'Betel que significa "casa de DEUS"'
+)
+
+criaCartao(
+    ' Espirito Santo',
+    'Quem é o Espirito Santo?',
+    'O consolador, que intercede por nós com gemidos inespremiveis, rm 8.26'
+)
+
+criaCartao(
+    'A criação do mundo',
+    'como Deus cirou o homem e a mulher?',
+    ' o homem ele criou do pó e o escupiu com amor, deu-lhe o sopro da vida, a mulher Deus criou diretamente da costela do homem, e a fez formosa e alegre'
+
+    ) 
+
+    criaCartao(
+        'Depois que Jesus ressucitou',
+        'Pedro negou Jesus três vezes por medo e angústia, após Jeusu ressucitar, ele se sentindo horrível foi pescar, o que aconteceu ali?',
+        ' Jesus apareceua ele e aos outros discipulos que ali estavam, Pedro nadou desesperado até Jesus e lhe pediu perdão, e Jesus com amor o perdoou, pois diferente de judasn Pedro se arrependeu'
+    
+    )
+    
